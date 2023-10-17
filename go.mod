de38c0defc0a8829e04419091ac744875cb860a1
Goh Yong Jie <gohyongjie1112@gmail.com>
2023-10-17 09:33:16 +0800

    feat: add enable raw mode and loop to read input

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/gohyongjie1112/GoEd
 
 go 1.20
+
+require (
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/term"
+)
+
+func enableRawMode() (*term.State, error) {
+	fd := int(os.Stdin.Fd())
+
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		return nil, err
+	}
+
+	return oldState, nil
+}
+
 func main() {
+	oldState, err := enableRawMode()
+	if err != nil {
+		fmt.Println("Error setting raw mode:", err)
+		return
+	}
+
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	fmt.Println("Terminal in raw mode. Press 'q' to quit.")
+
+	var c [1]byte
+	for {
+		_, err := os.Stdin.Read(c[:])
+		if err != nil || c[0] == 'q' {
+			break
+		}
+		fmt.Println("You pressed:", c[0])
+	}
 }
